3eb61b633abe5a3d15c1764eddd119ad1a01f831
Eduardo190202 <edu_190202@hotmail.com>
2025-08-21 10:51:10 -0600

    update

--- importar datos.R
+++ importar datos.R
@@ -1,3 +1,9 @@
+
+
+
+
+
+# importar datos ----------------------------------------------------------
 temperatura <- read.csv("C:/Repositorios/Posgrado_Estadistica_2025/temperatura.csv") 
 View(temperatura)
 
@@ -8,10 +14,37 @@ str(temperatura) #estructura del data frame
 
 summary(temperatura) #resumen estadistico
 
-names(temperatura) <- c("Anual","Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic") #corregir nombre de columna
+names(temperatura) <- c("Anual","Ene",
+                        "Feb","Mar","Abr","May","Jun","Jul","Ago","Sep",
+                        "Oct","Nov","Dic") #corregir nombre de columna
 
 temperatura$media_anual <- rowMeans(temperatura[,2:13])
 
 temperatura$media_anual <- rowMeans(temperatura[11:21,])
 
 head(temperatura)
+
+f
+
+temp <-  temperatura [ ,2:13]
+temperatura[2,2]
+
+boxplot(temperatura)
+
+colores <- c("navajowhite","salmon","red")
+
+boxplot(temp ,col = "salmon", main= "temperatura 2020:205",
+      xlab = "meses", 
+      ylab= "temperatura (c)")
+# importar datos web ------------------------------------------------------
+
+read.csv()
+url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv"
+
+
+senacia <-read.csv(url,header=T) 
+read.csv("https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv")
+
+senacica <-read.csv(url,header=T) 
+View(senacica)
+
